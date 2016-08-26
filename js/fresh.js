5afe27b06994eadcafc8217d1c22bb054f782e8b
Corbett Martin <corbett@corbs.local>
2016-08-25 21:57:47 -0500

    hover effects on text

--- js/fresh.js
+++ js/fresh.js
@@ -14,3 +14,21 @@ $('#main-menu__close').click(function() {
         duration: 500
     });
 });
+
+$('.main-menu__link').hover(function() {
+    // mouse enter
+    $(this).velocity({
+        color: '#1FC5FF',
+        translateZ: 1
+    }, {
+        duration: 250
+    });
+}, function() {
+    // mouse exit
+    $(this).velocity({
+        color: '#FA5BA4',
+        translateZ: 0
+    }, {
+        duration: 250
+    });
+});
